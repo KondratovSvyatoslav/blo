e662e81e7cd5fa4dce13f74d8cf0f05c0dec3fec
SvjatInc <svyatoslav.kondratov@sigma.software>
2019-04-07 12:58:44 +0300

    Updates

---
+++ precache-manifest.dce98ef85e600f690c44e145ebcafde9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "3082cb1fc05e58cd0f1e",
+    "url": "/blog/static/js/main.3da5f26b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/blog/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3082cb1fc05e58cd0f1e",
+    "url": "/blog/static/css/main.aee4dcef.chunk.css"
+  },
+  {
+    "revision": "dadedac19d641b8558e6a997c2cf9363",
+    "url": "/blog/index.html"
+  }
+];
