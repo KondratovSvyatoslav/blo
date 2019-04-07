f21bf3c28552f9f821aa3f574be9ef2c38f90f0a
SvjatInc <svyatoslav.kondratov@sigma.software>
2019-04-07 13:56:01 +0300

    Added two components

---
+++ src/Components/NextPageButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const nextPageButton = (props) => {
+    return <button className="nextPageButton">{props.children}</button>
+}
+
+export default nextPageButton
---
+++ src/Components/Person.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import logo from './../logo.svg';
+
+const Person = () => {
+    return (
+        <div className="Person">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Sed egestas egestas
+                fringilla phasellus faucibus. Duis ut diam quam nulla porttitor massa. Vel elit scelerisque mauris
+                pellentesque pulvinar pellentesque habitant morbi tristique. Pellentesque eu tincidunt tortor aliquam
+                nulla facilisi cras. Neque convallis a cras semper auctor. Mauris a diam maecenas sed enim ut sem
+                viverra aliquet. Id aliquet risus feugiat in. Volutpat est velit egestas dui id ornare arcu.</p>
+
+            <p>Semper eget duis at tellus at urna condimentum. Mollis aliquam ut porttitor leo a diam sollicitudin.
+                Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Vestibulum rhoncus est pellentesque
+                elit ullamcorper dignissim cras tincidunt lobortis. Sit amet commodo nulla facilisi nullam. Congue
+                quisque egestas diam in arcu cursus euismod. Morbi leo urna molestie at elementum eu. Amet mauris
+                commodo quis imperdiet massa tincidunt nunc pulvinar. Phasellus faucibus scelerisque eleifend donec
+                pretium vulputate sapien nec. Nunc sed blandit libero volutpat sed cras ornare. Feugiat vivamus at augue
+                eget arcu dictum varius. In egestas erat imperdiet sed euismod.</p>
+
+            <p>Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Ornare quam viverra orci sagittis eu
+                volutpat odio facilisis. Ut etiam sit amet nisl purus in mollis nunc. Lacus vel facilisis volutpat est
+                velit egestas dui id. Turpis cursus in hac habitasse. Bibendum neque egestas congue quisque egestas diam
+                in arcu cursus. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Cursus vitae congue
+                mauris rhoncus aenean vel elit scelerisque mauris. Mi tempus imperdiet nulla malesuada pellentesque.
+                Lectus mauris ultrices eros in cursus turpis massa tincidunt. Adipiscing commodo elit at imperdiet dui
+                accumsan sit amet. Fusce id velit ut tortor pretium viverra. Mauris augue neque gravida in fermentum et
+                sollicitudin ac. Vitae suscipit tellus mauris a diam maecenas sed. Ultrices mi tempus imperdiet nulla
+                malesuada pellentesque elit eget. Volutpat diam ut venenatis tellus in.</p>
+
+            <p>Sit amet nisl suscipit adipiscing bibendum est ultricies integer. Quis commodo odio aenean sed. Facilisis
+                mauris sit amet massa. Consectetur adipiscing elit ut aliquam purus sit. Euismod elementum nisi quis
+                eleifend quam adipiscing. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. In ante metus
+                dictum at tempor commodo. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas.
+                Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Leo integer
+                malesuada nunc vel risus commodo. Venenatis tellus in metus vulputate. Sollicitudin tempor id eu nisl
+                nunc mi ipsum. Consectetur a erat nam at. Et egestas quis ipsum suspendisse ultrices. Fermentum iaculis
+                eu non diam phasellus vestibulum lorem sed. Condimentum mattis pellentesque id nibh tortor. Ac turpis
+                egestas sed tempus. Proin gravida hendrerit lectus a. Vitae auctor eu augue ut lectus arcu bibendum at
+                varius. Malesuada fames ac turpis egestas.</p>
+
+            <p>Consectetur a erat nam at. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla. Id
+                consectetur purus ut faucibus. Neque aliquam vestibulum morbi blandit. Augue ut lectus arcu bibendum at
+                varius vel. Enim eu turpis egestas pretium aenean pharetra magna. Magnis dis parturient montes nascetur
+                ridiculus mus mauris vitae ultricies. Maecenas sed enim ut sem viverra aliquet eget. Pharetra et
+                ultrices neque ornare aenean. Posuere ac ut consequat semper viverra. Ut tortor pretium viverra
+                suspendisse potenti nullam ac. Nunc non blandit massa enim nec dui nunc mattis enim. Ultricies leo
+                integer malesuada nunc vel risus commodo viverra maecenas. Proin nibh nisl condimentum id venenatis a.
+                Diam in arcu cursus euismod. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor
+                posuere. Volutpat ac tincidunt vitae semper quis lectus nulla. Nunc aliquet bibendum enim facilisis
+                gravida neque convallis a cras. Massa tempor nec feugiat nisl pretium fusce id.</p>
+        </div>
+    )
+}
+
+export default Person
